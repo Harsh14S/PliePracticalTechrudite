d10a7fcececed3d771fe7e0f69b5c6a397d5ab4f
Harsh14S <harsh105savaliya@gmail.com>
2025-04-25 18:14:01 +0530

    Removed unnecessary files

--- src/utils/data/SubscriptionCycle.tsx
+++
@@ -1,8 +0,0 @@
-export const SUBSCRIPTION_CYCLE: {cycle: string}[] = [
-  {cycle: 'Daily'},
-  {cycle: 'Weekly'},
-  {cycle: 'Monthly'},
-  {cycle: 'Quarterly'},
-  {cycle: 'Annually'},
-  {cycle: 'Custom'},
-];
